b9ece0ece267cac9d0c96c2a19cfd4edfe718e81
somtola <mac@somtola.local>
2024-08-06 20:12:23 +0700

    fist commit by somtola

---
+++ lang/en/home.php
@@ -0,0 +1,17 @@
+<?php
+
+    return[
+        'title' => 'Fast & Reliable Deliveries',
+        'sub_title' => 'You Can Trust',
+        'des' => "Experience top-notch delivery services with Everex Express. We offer swift, cost-effective solutions tailored to meet your business needs. Spend more time doing what you more - we'll take care your every parcel.",
+        'title_one' => 'Same-Day Delivery',
+        'sub_title_one' => 'Get your parcels delivered within the same day, ensuring prompt service and customer satisfaction.',
+        'title_two' => ' Affordable Pricing',
+        'sub_title_two' => 'Enjoy competitive rates and cost-effective delivery options tailored to your budget.',
+        'title_three' => 'Real-Time Tracking',
+        'sub_title_three' => 'Stay informed with real-time tracking and instant notifications for every delivery.',
+        'title_four' => 'Reliable Customer Support',
+        'sub_title_four' => 'Benefit from our dedicated support team, available to assist you every step of the way.',
+
+
+    ];
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,39 @@
+
+<footer class="bg-gray-900 text-white dark:bg-gray-900">
+    <div class="pl-10 mx-auto w-full max-w-screen-xl">
+      <div class="grid md:p-10 grid-cols-2 grap:4 gap-4 md:gap-8 py-6 lg:py-8 md:grid-cols-4">
+        <div>
+            <ul class="text-white dark:text-white font-medium">
+                <li class="mb-4">
+                    <a href="{{ url('/') }}" class="hover:underline">Home</a>
+                </li>
+                <li class="mb-4">
+                    <a href="{{ url('/about') }}" class="hover:underline  ">About</a>
+                </li>
+                <li class="mb-4">
+                    <a href="{{ url('/pricing') }}" class="hover:underline  ">Pricing</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <ul class="text-white dark:text-white font-medium">
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Contact Us</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Tiktok</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Facebook</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Privacy Policy</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">Terms & Conditions</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
